e003161d1c4ee0b44eaefb2925a9244883101898
Ryan Fournier <ryanfour1637@gmail.com>
2023-11-01 18:50:45 -0700

    removed unique constraint

--- app/models/bytestream.py
+++ app/models/bytestream.py
@@ -22,10 +22,6 @@ class Bytestream(db.Model):
     ## BytestreamUser Relationship
     bytestreamuser_bytestream = db.relationship('BytestreamUser', back_populates='bytestream_bytestreamuser', cascade='all, delete-orphan')
 
-    ## Unique Constraint to ensure no duplicate names across the same bytestream.
-    __table_args__ = (
-        UniqueConstraint('name', 'bytespace_id', name='uq_name_bytespace_id'),
-    )
 
     def to_dict(self):
         return {
@@ -34,4 +30,43 @@ class Bytestream(db.Model):
             'ownerId': self.owner_id,
             'bytespaceId': self.bytespace_id,
             'dateCreated': self.date_created
+        }
+
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class BytestreamUser(db.Model):
+    __tablename__ = 'bytestream_users'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    bytestream_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('bytestreams.id')))
+    bytespace_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('bytespaces.id')))
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+
+    ## User Relationships
+    user_bytestreamuser = db.relationship('User', back_populates='bytestreamuser_user')
+
+    ## Bytespace Relationships
+    bytespace_bytestreamuser = db.relationship('Bytespace', back_populates='bytestreamuser_bytespace')
+
+    ## Bytestream Relationships
+    bytestream_bytestreamuser = db.relationship('Bytestream', back_populates='bytestreamuser_bytestream')
+
+
+
+
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'bytestreamId': self.bytestream_id,
+            'bytespaceId': self.bytespace_id,
+            'userId': self.user_id,
+            'username': self.user_username,
+            'firstName': self.user_first_name,
+            'lastName': self.user_last_name,
+            'email': self.user_email
         }
