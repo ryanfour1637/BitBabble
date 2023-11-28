c44a3207b939279c68b3c2c1205684c6dad1b0b5
Ryan Fournier <ryanfour1637@gmail.com>
2023-11-28 11:24:41 -0800

    got add to bytespace working, just need to filter the list for bytespaces a user has already joined to ensure they cannot join twice

--- react-app/src/components/Leftside-Navbar/joinBytespaceModal.js
+++ react-app/src/components/Leftside-Navbar/joinBytespaceModal.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { login } from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkGetAllBytespaces } from "../../store/bytespace";
+import { thunkAddToBytespace } from "../../store/bytespace_members";
 import { useModal } from "../../context/Modal";
 
 function JoinBytespaceModal() {
@@ -10,25 +11,36 @@ function JoinBytespaceModal() {
    const { closeModal } = useModal();
    const [errors, setErrors] = useState([]);
    const [selectedValue, setSelectedValue] = useState("");
-
+   const [selectedId, setSelectedId] = useState(null);
    const bytespacesArr = Object.values(bytespaces);
+
+   // need to filter out bytespaces which a particular user is already a part of so that they cannot be added to it a second time.
+   // const bytespacesToDisplay =
+   const selectedBytespace = bytespacesArr.find(
+      (bytespace) => bytespace.id == selectedId
+   );
 
    useEffect(() => {
       dispatch(thunkGetAllBytespaces());
    }, [dispatch]);
 
-   const joinBytespace = () => {};
+   const valueChange = (e) => {
+      setSelectedId(e.target.value);
+      setSelectedValue(selectedBytespace);
+   };
+
+   const joinBytespace = () => {
+      console.log("onclick", selectedId);
+      dispatch(thunkAddToBytespace(selectedId));
+   };
 
    return (
       <div>
          <h1>Join Bytespaces</h1>
-         <select
-            value={selectedValue}
-            onChange={(e) => setSelectedValue(e.target.value)}
-         >
+         <select value={selectedValue} onChange={valueChange}>
             <option value="">Select a bytespace</option>
             {bytespacesArr.map((bytespace) => (
-               <option key={bytespace.id} value={bytespace.name}>
+               <option key={bytespace.id} value={bytespace.id}>
                   {bytespace.name}
                </option>
             ))}
