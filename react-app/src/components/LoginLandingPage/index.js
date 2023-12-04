3c7ca9954d90fab8cc339559b4b74d0015ee1532
Ryan Fournier <ryanfour1637@gmail.com>
2023-12-04 12:04:32 -0800

    added functionality to display only the bytespaces you are in upon login

--- react-app/src/components/LoginLandingPage/index.js
+++ react-app/src/components/LoginLandingPage/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams, NavLink } from "react-router-dom";
 import { thunkGetAllBytespaces } from "../../store/bytespace";
+import { thunkGetAllMembers } from "../../store/bytespace_members";
 import OpenModalButton from "../OpenModalButton";
 import JoinBytespaceModal from "../Leftside-Navbar/joinBytespaceModal";
 
@@ -10,16 +11,39 @@ function LoginLandingPage() {
    const user = useSelector((state) => state.session.user);
    const bytespaces = useSelector((state) => state.bytespace.bytespaces);
    const bytespacesArr = Object.values(bytespaces);
-   // going to need to filter this for the bytespace list once I have the store built out for bytespaceUsers so that I can show all bytespaces a person is in, not just the ones they started.
+   const bytespacesMembershipRosters = useSelector(
+      (state) => state.bytespaceMembers
+   );
+   const bytespacesMembershipRostersArr = Object.entries(
+      bytespacesMembershipRosters
+   );
 
+   const joinedBytespaceArr = [];
+
+   for (let rosterData of bytespacesMembershipRostersArr) {
+      const [bytespaceId, roster] = rosterData;
+
+      if (Object.keys(roster).includes(user.id.toString())) {
+         joinedBytespaceArr.push(bytespaceId);
+      }
+   }
+
+   const bytespacesToDisplay = [];
+
+   for (let bytespace of bytespacesArr) {
+      if (joinedBytespaceArr.includes(bytespace.id.toString())) {
+         bytespacesToDisplay.push(bytespace);
+      }
+   }
    useEffect(() => {
       dispatch(thunkGetAllBytespaces());
+      dispatch(thunkGetAllMembers());
    }, [dispatch]);
 
    return (
       <div>
          {bytespacesArr.length > 0 ? (
-            bytespacesArr.map((bytespace) => (
+            bytespacesToDisplay.map((bytespace) => (
                <NavLink to={`/user/${user.id}/bytespaces/${bytespace.id}`}>
                   <h2>{bytespace.name}</h2>
                </NavLink>
