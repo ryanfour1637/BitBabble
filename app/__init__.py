f781f6d9aeef365ef85d7f8f39f433b0a6ea8d94
Ryan Fournier <ryanfour1637@gmail.com>
2023-12-15 07:59:35 -0800

    started messages section and built backend routes for websocket

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required, current_user
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
@@ -97,3 +98,82 @@ def react_root(path):
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
+
+socketio = SocketIO(app)
+
+@socketio.on('join_bytestream')
+def handle_join_bytestream(data):
+    join_room(data['bytestream_id'])
+    emit('join_stream', data, room=data['bytestream_id'])
+
+@socketio.on('leave_bytestream')
+def handle_leave_bytestream(data):
+    leave_room(data['bytestream_id'])
+    emit('leave_stream', data, room=data['bytestream_id'])
+
+@socketio.on('send_message')
+def handle_send_message(data):
+    bytestream = data['bytestream_id']
+    message = data['message']
+    emit('receive_message', {'message': message}, room=bytestream)
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('typing')
+def handle_typing(data):
+    bytestream = data['bytestream_id']
+    user=data['user']
+    emit('user_typing', {'user': user}, room=bytestream)
+
+@socketio.on('error')
+def handle_error(e):
+    print(e)
+
+@socketio.on('connect')
+def handle_connect():
+    user_id = get_user_id()  # Your method to identify the connected user
+    user = User.query.get(user_id)
+    if user:
+        user.is_online = True
+        db.session.commit()
+        emit('user_status_change', {'user_id': user_id, 'status': 'online'}, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    user_id = b 
+    user = User.query.get(user_id)
+    if user:
+        user.is_online = False
+        db.session.commit()
+        emit('user_status_change', {'user_id': user_id, 'status': 'offline'}, broadcast=True)
+
+
+# @socketio.on('send_file')
+# def handle_send_file(data):
+#     bytestream = data['bytestream_id']
+#     file = data['file']
+#     emit('receive_file', {'file': file}, room=bytestream)
+
+# @socketio.on('send_image')
+# def handle_send_image(data):
+#     bytestream = data['bytestream_id']
+#     image = data['image']
+#     emit('receive_image', {'image': image}, room=bytestream)
+
+# @socketio.on('send_video')
+# def handle_send_video(data):
+#     bytestream = data['bytestream_id']
+#     video = data['video']
+#     emit('receive_video', {'video': video}, room=bytestream)
+
+# @socketio.on('send_audio')
+# def handle_send_audio(data):
+#     bytestream = data['bytestream_id']
+#     audio = data['audio']
+#     emit('receive_audio', {'audio': audio}, room=bytestream)
