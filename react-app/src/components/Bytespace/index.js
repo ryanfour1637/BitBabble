9bd2d2343f1f94007ee61ef72b09d23860de4fa3
Ryan Fournier <ryanfour1637@gmail.com>
2024-01-10 18:16:23 -0500

    adding bootstrap import to the correct spot

--- react-app/src/components/Bytespace/index.js
+++ react-app/src/components/Bytespace/index.js
@@ -7,7 +7,7 @@ import Navigation from "../Navigation";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./singlebytespace.css";
 
 function SingleBytespaceLandingPage({ user }) {
