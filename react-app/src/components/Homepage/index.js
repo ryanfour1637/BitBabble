53b26c28dd50d311e99df477e7777acfc7f9b3bc
Ryan Fournier <ryanfour1637@gmail.com>
2024-01-12 15:19:15 -0500

    fixing homepage

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -5,59 +5,75 @@ import OpenModalButton from "../OpenModalButton";
 import SignupFormModal from "../SignupFormModal";
 import LoginFormModal from "../LoginFormModal";
 import loggedoutimage from "../../images/loggedout-homepage-image.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./homepage.css";
 
 function LoggedOutLandingPage() {
    return (
-      <div className="loggedout-wholepage">
-         <div className="loggedout-topdiv">
-            <div className="loggedout-topdiv-leftside">
-               <p className="loggedout-topdiv-leftside-links">
-                  About this project
-               </p>
-               <p className="loggedout-topdiv-leftside-links">About Me</p>
-               <p className="loggedout-topdiv-leftside-links">Contact me?</p>
-            </div>
-            <div className="loggedout-topdiv-rightside">
-               <OpenModalButton
-                  className="joinNowButton"
-                  buttonText="Join Now"
-                  modalComponent={<SignupFormModal />}
-               />
-
-               <OpenModalButton
-                  buttonText="Log In"
-                  modalComponent={<LoginFormModal />}
-               />
-            </div>
-         </div>
-         <h1 className="loggedout-midpage">How does BitBabble work?</h1>
-         <div className="loggedout-bottompage">
-            <div className="loggedout-bottompage-leftside">
-               <h2 className="loggedout-bottompage-leftside-gettingstarted">
-                  Getting started with BitBabble
-               </h2>
-               <p className="loggedout-bottompage-leftside-paragraph">
-                  As a new user, you can create and manage workspaces and
-                  channels on BitBabble. Sign up as a new user to generate your
-                  first workspace or to join a public one. You can always create
-                  another whenever you need one!
-               </p>
-            </div>
-            <div className="loggedout-bottompage-rightside">
+      <Container fluid className="h-100 loggedout-wholepage">
+         <Row className="homepage-toprow">
+            <Col>
+               <Row>
+                  <Col>
+                     <a>About this Project</a>
+                  </Col>
+                  <Col>
+                     <a>About Me</a>
+                  </Col>
+               </Row>
+            </Col>
+            <Col></Col>
+            <Col>
+               <Row>
+                  <Col>
+                     <OpenModalButton
+                        className="joinNowButton"
+                        buttonText="Join Now"
+                        modalComponent={<SignupFormModal />}
+                     />
+                  </Col>
+                  <Col>
+                     <OpenModalButton
+                        buttonText="Log In"
+                        modalComponent={<LoginFormModal />}
+                     />
+                  </Col>
+               </Row>
+            </Col>
+         </Row>
+         <Row className="homepage-h1">
+            <h1>How does BitBabble work</h1>
+         </Row>
+         <Row className="homepage-h2-img">
+            <Col>
+               <Row>
+                  <h2>Getting started with BitBabble</h2>
+                  <p>
+                     {" "}
+                     As a new user, you can create and manage workspaces and
+                     channels on BitBabble. Sign up as a new user to generate
+                     your first workspace or to join a public one. You can
+                     always create another whenever you need one!
+                  </p>
+               </Row>
+               <Row></Row>
+            </Col>
+            <Col>
                <img
                   src={loggedoutimage}
                   alt="bytespaces and bytestreams"
                   id="loggedout-image"
                />
-            </div>
-         </div>
-         <div className="bottom-footer">
+            </Col>
+         </Row>
+         <Row className="homepage-footer">
             <p className="bottom-footer-tags">LinkedIn placeholder</p>
             <p className="bottom-footer-tags">GitHub placeholder</p>
             <p className="bottom-footer-tags">download resume placeholder</p>
-         </div>
-      </div>
+         </Row>
+      </Container>
    );
 }
 
