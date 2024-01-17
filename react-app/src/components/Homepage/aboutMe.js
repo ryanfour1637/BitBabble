98f78d7862145a97d481614a16a63a98226bed3b
Ryan Fournier <ryanfour1637@gmail.com>
2024-01-17 16:17:43 -0500

    added about me and about project

---
+++ react-app/src/components/Homepage/aboutMe.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+
+function AboutMe() {
+   return (
+      <Col className="about-me-div">
+         <h1>About Me</h1>
+         <p>
+            My foray into software development began unexpectedly during a data
+            analytics course, where I first delved into programming with the
+            language, R. This introduction sparked a profound interest in
+            coding, compelling me to explore further. My journey in this field,
+            characterized by structured thinking and an innate ability to
+            prioritize, has been a blend of self-discovery and skill refinement.
+            In managing tasks, I focus on addressing the most critical aspects
+            of my work first and also on identifying and resolving bottlenecks
+            in group projects. This approach ensures that my contribution
+            enhances team efficiency, allowing my team members to advance their
+            work without delay. My commitment to efficiency has deepened my
+            understanding of software development, a domain where continuous
+            learning and skill enhancement are integral. The dynamic nature of
+            this field keeps me engaged and motivated for continual growth, as
+            each new programming tool, library or method I learn opens up novel
+            and more efficient ways of problem-solving. My journey so far has
+            been immensely rewarding, and I eagerly anticipate the continued
+            evolution of my professional capabilities.
+            <br></br>
+            <br></br>
+            In the workplace, I bring a balanced mix of non-technical and
+            technical strengths. My effective communication with stakeholders at
+            various levels, coupled with an ability to grasp the overarching
+            strategic picture, ensures that my role is well-integrated within
+            the broader objectives of the organization. As a team player who
+            values positive and productive collaboration, I thrive in
+            environments that foster strong relationships and teamwork. I take
+            pride in my ability to contribute innovative solutions and ideas,
+            rather than problems and complaints, to ensure continued progress
+            unhindered by avoidable obstacles. This trait, which I also admire
+            in others, is pivotal in creating a dynamic and forward-moving work
+            environment. The most gratifying part of my journey is when a new
+            skill or concept becomes a seamless part of my daily work routine.
+            Beyond my professional interests, my family, including my partner,
+            Zoila, young son, Charlie, and dog, Bergy, in addition to hockey and
+            football provide me an outlet for relaxation and enjoyment.
+         </p>
+      </Col>
+   );
+}
+
+export default AboutMe;
