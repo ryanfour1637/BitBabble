8592b364330276e1a2296d55fc1af8b2cb5c07ab
Ryan Fournier <ryanfour1637@gmail.com>
2024-02-05 16:16:44 -0500

    small fix to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager, login_required, current_user
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
-from .models import db, User
+from .models import db, User, Message
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.bytespace_routes import bytespace_routes
@@ -14,7 +14,6 @@ from .api.bytespace_members import bytespace_members_routes
 from .api.bytestream_members import bytestream_members_routes
 from .api.bytestream_routes import bytestream_routes
 from .api.messages_routes import message_routes
-from .models import User, Message
 from .seeds import seed_commands
 from .config import Config
 
